15a4ef9b9e69b5d1171e29cfae837ba779669128
Spencer Jones <spencerj@swotservd.ldeo.columbia.edu>
2022-02-28 16:22:34 -0500

    divide by h

--- redi_analytical.py
+++ redi_analytical.py
@@ -9,23 +9,24 @@ def redi_simple(lat,kappa,v,La,modelh,ntime,dt):
     Cs[119:]=np.ones(25)
 
     dy=108086
-    #note that walt has units m^2/s because it is actually w*La
-    watl=-np.diff(v)/dy
+    #note that walt has units m/s because it is actually w
+    watl=-np.diff(v)/dy/La[:-1]
+    #v has units m^3/s
 
     kappav=2*10**-5
 
     timestep=dt*24*3600*365
     for time in range(1, ntime):
         Cnew[1:-1]=(Cold[1:-1]+
-                timestep*((kappa[2:]*La[2:]*Cold[2:]*(modelh[1:-1]+modelh[2:])/modelh[2:]/2
-                           -kappa[2:]*La[2:]*Cold[1:-1]*(modelh[1:-1]+modelh[2:])/modelh[1:-1]/2- 
-                           kappa[1:-1]*La[1:-1]*Cold[1:-1]*(modelh[1:-1]+modelh[0:-2])/modelh[1:-1]/2
-                           +kappa[1:-1]*La[1:-1]*Cold[0:-2]*(modelh[1:-1]+modelh[0:-2])/modelh[0:-2]/2)/dy**2/La[1:-1]
-                          -(v[2:]*(Cold[2:]+Cold[1:-1])/modelh[2:]-v[1:-1]*(Cold[1:-1]+Cold[0:-2])/modelh[1:-1])/dy/2/La[1:-1]
-                           -watl[1:]*Cold[1:-1]/modelh[1:-1]/La[1:-1]+kappav*(Cs[1:-1]-Cold[1:-1]/modelh[1:-1])/modelh[1:-1]))#
+                timestep*((kappa[2:]*La[2:]*Cold[2:]*(modelh[1:-1]+modelh[2:])/2
+                           -kappa[2:]*La[2:]*Cold[1:-1]*(modelh[1:-1]+modelh[2:])/2- 
+                           kappa[1:-1]*La[1:-1]*Cold[1:-1]*(modelh[1:-1]+modelh[0:-2])/2
+                           +kappa[1:-1]*La[1:-1]*Cold[0:-2]*(modelh[1:-1]+modelh[0:-2])/2)/dy**2/La[1:-1]/modelh[1:-1]
+                          -(v[2:]*(Cold[2:]+Cold[1:-1])-v[1:-1]*(Cold[1:-1]+Cold[0:-2]))/dy/2/La[1:-1]/modelh[1:-1]
+                           -watl[1:]*Cold[1:-1]/modelh[1:-1]+kappav*(Cs[1:-1]-Cold[1:-1])/modelh[1:-1]/modelh[1:-1]))#
         #Boundary conditions are set here. 
         Cnew[0]=0
-        Cnew[-1]=modelh[-1]
+        Cnew[-1]=1
         Cold=Cnew
         timeend=time-ntime+40000
         if timeend>=0:
